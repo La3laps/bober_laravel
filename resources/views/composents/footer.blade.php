7306989abcb0fa50a0203150b64f18f935625874
r.legrand <remi.legrand@le-campus-numerique.fr>
2024-07-31 11:35:32 +0200

    Styled footer

--- resources/views/composents/footer.blade.php
+++ resources/views/composents/footer.blade.php
@@ -1,25 +1,92 @@
-<footer>
-    <div class="footer">
-        <div class="container">
-            <div class="col">
-                <h1>Company</h1>
-                <ul>
-                    <li>About</li>
-                    <li>Mission</li>
-                    <li>Services</li>
-                    <li>Social</li>
-                    <li>Get in touch</li>
-                </ul>
-            </div>
-            <div class="col">
-                <h1>Support</h1>
-                <ul>
-                    <li>Contact us</li>
-                    <li>Web chat</li>
-                </ul>
-            </div>
+<footer class="bg-light-subtle text-center text-lg-start fixed-bottom fs-5 ">
+    <!-- Grid container -->
+    <div class="container p-4">
+      <!--Grid row-->
+      <div class="row">
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Company</h5>
+  
+          <ul class="list-unstyled mb-0" style="font-size: 15px;">
+            <li>
+              <a href="#!" class="text-dark">About</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Mission</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Services</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Social</a>
+            </li>
+            <li>
+                <a href="#!" class="text-dark">Get in touch</a>
+            </li>
+          </ul>
         </div>
+  
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase mb-0">Support</h5>
+  
+          <ul class="list-unstyled" style="font-size: 15px;">
+            <li>
+              <a href="#!" class="text-dark">Contact us</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Web chat</a>
+            </li>
+          </ul>
+        </div>
+  
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Projects</h5>
+  
+          <ul class="list-unstyled mb-0" style="font-size: 15px;">
+            <li>
+              <a href="#!" class="text-dark">Planes & planes</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Plane art application</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">National Museum for Planes</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Planes in the dark: an ominous expedition</a>
+            </li>
+          </ul>
+        </div>
+  
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase mb-0">Community</h5>
+  
+          <ul class="list-unstyled" style="font-size: 15px;">
+            <li>
+              <a href="#!" class="text-dark">Issues</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Discussions</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Corporate Sponsors</a>
+            </li>
+            <li>
+              <a href="#!" class="text-dark">Stack overflow</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-</footer>
+  
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.01);">
+      © 2020 Copyright:
+      <span class="text-dark">Corentin, Gabriela, Julien, Rémi.</span>
+    </div>
+  </footer>
 </body>
 </html>
