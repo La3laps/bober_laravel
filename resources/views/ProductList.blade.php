a1f4250e3edb280e41f256862a9ad83e54260528
Gaby <gabriela.cigerova@le-campus-numerique.fr>
2024-07-31 14:05:08 +0200

    Visual changes

--- resources/views/ProductList.blade.php
+++ resources/views/ProductList.blade.php
@@ -11,23 +11,26 @@
         <img src="https://onsefaituncine.com/wp-content/uploads/2020/06/porco-rosso1.jpg" alt="Porco Rosso plane">
         <h2>Red plane</h2>
         <p><em>Functional and very authentic (not a replica!) plane belonging to Porco Rosso, a god amongst porks.</em></p>
-    </div>
+    </div></div>
+<div class="container">
     <div class="product">
         <img src="https://m.media-amazon.com/images/M/MV5BNmQ2ZWMwMzgtZjZjMS00ZTY0LWE3YjEtMDc3YmNkMWFkNDMwXkEyXkFqcGdeQVRoaXJkUGFydHlJbmdlc3Rpb25Xb3JrZmxvdw@@._V1_.jpg" alt="The Wind Rises plane">
         <h2>French-colored plane</h2>
         <p><em>If you look at the plane from the left-hand side, it looks like the French flag.  Cocorico!</em></p>
-    </div>
+    </div></div>
+<div class="container">
     <div class="product">
         <img src="https://www.economist.com/cdn-cgi/image/width=1424,quality=80,format=auto/sites/default/files/images/print-edition/20200606_BKP001_0.jpg" alt="Kiki's broom">
         <h2>Kiki's magic broom</h2>
         <p><em>It's not a plane, but you get the idea.  It floats in the air.  Neko not included.</em></p>
-    </div>
+    </div></div>
+<div class="container">
     <div class="product">
         <img src="https://www.studioghibli.fr/wp-content/uploads/2009/07/nausicaa1.jpg" alt="Nausicaä's glider">
         <h2>Nausicaä's glider</h2>
         <p><em>It is powered by some strange magic; the smallest gust of wind will send this baby into the stratosphere *slaps hood of glider*</em></p>
-    </div>
-</div>
+    </div></div>
+
 </body>
 
 @include('composents.footer')
